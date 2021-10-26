19be226eed7d00ec10f5d33fd36f3af8f6e349d2
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2021-10-26 15:14:37 +0300

    delete

--- list/list/listFunctions.c
+++ list/list/listFunctions.c
@@ -1,4 +1,6 @@
 #include "listFunctions.h"
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct ListElement
@@ -6,15 +8,23 @@ typedef struct ListElement
     int value;
     struct ListElement* previous;
     struct ListElement* next;
-};
+}ListElement;
 
 typedef struct DoubleList
 {
-    struct ListElement* list;
-};
+    struct ListElement* head;
+}DoubleList;
 
 typedef struct Position
 {
     struct ListElement* position;
-};
+}Position;
 
+void delete(DoubleList* list, Position* position)
+{
+    Position* iterator = list->head;
+    while (!isEnd(iterator))
+    {
+        remove(list, position);
+    }
+}
---
+++ list/list/listFunctions.h
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef struct DoubleList DoubleList;
+
+typedef struct Position Position;
+
+void delete(DoubleList* list, Position* position);
