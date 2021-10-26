6bf411ebe1ee315bcb1ddb9ed8fee378d5b44ddf
ArtiomPatov <patov.988@gmail.com>
2021-10-26 14:13:22 +0300

    added structs

--- list/list/listFunctions.c
+++ list/list/listFunctions.c
@@ -1,2 +1,20 @@
 #include "listFunctions.h"
 #include <stdlib.h>
+
+typedef struct ListElement
+{
+    int value;
+    struct ListElement* previous;
+    struct ListElement* next;
+};
+
+typedef struct DoubleList
+{
+    struct ListElement* list;
+};
+
+typedef struct Position
+{
+    struct ListElement* position;
+};
+
